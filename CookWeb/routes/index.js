06988faa4f9c3378f5fe4ea92afac02a27053862
zhangmike <micezzyy@gmail.com>
2013-12-25 22:05:27 -0500

    add plans

--- CookWeb/routes/index.js
+++ CookWeb/routes/index.js
@@ -5,9 +5,11 @@
 
 module.exports = function(app){
   
-  app.get('/',function (req,res){res.render('index',{title:'食之康'});});
-  app.get('/menu',function(req,res){res.render('menu',{title:'食之康'});});
-  app.get('/health',function(req,res){res.render('health',{title:'食之康'});});
-  app.get('/talk',function(req,res){res.render('talk',{title:'食之康'});});
-  app.get('/login',function(req,res){res.render('login',{title:'食之康'});});
+  app.get('/',function (req,res){res.render('index');});
+  app.get('/menu',function(req,res){res.render('menu');});
+  app.get('/health',function(req,res){res.render('health');});
+  app.get('/talk',function(req,res){res.render('talk');});
+  app.get('/login',function(req,res){res.render('login');});
+  app.get('/farm',function(req,res){res.render('farm');});
+  app.get('/cook',function(req,res){res.render('cook')})
 };
